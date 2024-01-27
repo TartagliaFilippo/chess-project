a1c2e3cf5f2b233127dd269ac2bdf1231f6b9122
filippo199600 <pippotartaglia@gmail.com>
2024-01-27 12:09:49 +0100

    edd ids elemnet img and boxes

--- pieces.js
+++ pieces.js
@@ -11,66 +11,82 @@ const whiteBishop = document.querySelectorAll(".white-bishop");
 const whiteQueen = document.querySelector(".white-queen");
 const whiteKing = document.querySelector(".white-king");
 
-blackPawn.forEach(function (pawn) {
+// PEZZI NERI
+
+blackPawn.forEach(function (pawn, index) {
   const pawnElement = document.createElement("img");
   pawnElement.src = "./img/chess-pieces/black-pawn.svg";
+  pawnElement.id = "black-pawn-" + index;
   pawn.appendChild(pawnElement);
 });
 
-blackRook.forEach(function (rook) {
+blackRook.forEach(function (rook, index) {
   const rookElement = document.createElement("img");
   rookElement.src = "./img/chess-pieces/black-rook.svg";
+  rookElement.id = "black-rook-" + index;
   rook.appendChild(rookElement);
 });
 
-blackKnight.forEach(function (knight) {
+blackKnight.forEach(function (knight, index) {
   const knightElement = document.createElement("img");
   knightElement.src = "./img/chess-pieces/black-knight.svg";
+  knightElement.id = "black-knight-" + index;
   knight.appendChild(knightElement);
 });
 
-blackBishop.forEach(function (bishop) {
+blackBishop.forEach(function (bishop, index) {
   const bishopElement = document.createElement("img");
   bishopElement.src = "./img/chess-pieces/black-bishop.svg";
+  bishopElement.id = "black-bishop-" + index;
   bishop.appendChild(bishopElement);
 });
 
 const blackQueenElement = document.createElement("img");
 blackQueenElement.src = "./img/chess-pieces/black-queen.svg";
+blackQueenElement.id = "black-queen";
 blackQueen.appendChild(blackQueenElement);
 
 const blackKingElement = document.createElement("img");
 blackKingElement.src = "./img/chess-pieces/black-king.svg";
+blackKingElement.id = "black-king";
 blackKing.appendChild(blackKingElement);
 
-whitePawn.forEach(function (pawn) {
+// PEZZI BIANCHI
+
+whitePawn.forEach(function (pawn, index) {
   const pawnElement = document.createElement("img");
   pawnElement.src = "./img/chess-pieces/white-pawn.svg";
+  pawnElement.id = "white-pawn-" + index;
   pawn.appendChild(pawnElement);
 });
 
-whiteRook.forEach(function (rook) {
+whiteRook.forEach(function (rook, index) {
   const rookElement = document.createElement("img");
   rookElement.src = "./img/chess-pieces/white-rook.svg";
+  rookElement.id = "white-rook-" + index;
   rook.appendChild(rookElement);
 });
 
-whiteKnight.forEach(function (knight) {
+whiteKnight.forEach(function (knight, index) {
   const knightElement = document.createElement("img");
   knightElement.src = "./img/chess-pieces/white-knight.svg";
+  knightElement.id = "white-knight-" + index;
   knight.appendChild(knightElement);
 });
 
-whiteBishop.forEach(function (bishop) {
+whiteBishop.forEach(function (bishop, index) {
   const bishopElement = document.createElement("img");
   bishopElement.src = "./img/chess-pieces/white-bishop.svg";
+  bishopElement.id = "white-bishop-" + index;
   bishop.appendChild(bishopElement);
 });
 
 const whiteQueenElement = document.createElement("img");
 whiteQueenElement.src = "./img/chess-pieces/white-queen.svg";
+whiteQueenElement.id = "white-queen";
 whiteQueen.appendChild(whiteQueenElement);
 
 const whiteKingElement = document.createElement("img");
 whiteKingElement.src = "./img/chess-pieces/white-king.svg";
+whiteKingElement.id = "white-king";
 whiteKing.appendChild(whiteKingElement);
